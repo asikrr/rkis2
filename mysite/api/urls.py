222696cc57946945ceb92be7b37a6e7404492faf
Anastasiia <asav32048@gmail.com>
2025-10-26 22:32:09 +0700

    У админа появилась возможность добавлять книги и авторов

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('authors/', views.AuthorListCreate.as_view()),
-    path('books/', views.BookListCreate.as_view()),
+    path('authors/', views.AuthorList.as_view()),
+    path('books/', views.BookList.as_view()),
 ]
--- mysite/api/views.py
+++ mysite/api/views.py
@@ -3,10 +3,10 @@ from rest_framework import generics
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
 
-class AuthorListCreate(generics.ListCreateAPIView):
+class AuthorList(generics.ListAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
 
-class BookListCreate(generics.ListCreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+class BookList(generics.ListAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer

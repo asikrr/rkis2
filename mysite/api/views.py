59dce4970e543a57f90d2bffd67fcb7640fec655
Anastasiia <asav32048@gmail.com>
2025-10-26 22:03:19 +0700

    Создание представлений для автора и книги

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
 
-# Create your views here.
+class AuthorListCreate(generics.ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class BookListCreate(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer

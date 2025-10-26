8f3363e99af39b529a13bd2f829ce00df84fe274
Anastasiia <asav32048@gmail.com>
2025-10-26 21:17:49 +0700

    Создание моделей для автора и книги

--- mysite/api/models.py
+++ mysite/api/models.py
@@ -1,3 +1,27 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    patronymic = models.CharField(max_length=100, blank=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    pub_year = models.IntegerField(validators=[MinValueValidator(1000), MaxValueValidator(9999)])
+    genre = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    publisher = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
+    file = models.FileField(upload_to='files/')
+
+    class Meta:
+        unique_together = ('title', 'author', 'pub_year', 'publisher')
+
+    def __str__(self):
+        return self.title

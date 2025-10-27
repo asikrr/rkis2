372620a00a48c0f53be1d655b88661a38b46a1c7
Anastasiia <asav32048@gmail.com>
2025-10-27 10:25:36 +0700

    Добавила поиск

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, filters
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
 
-class AuthorList(generics.ListAPIView):
+class AuthorList(generics.ListCreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['last_name', 'first_name']
 
-class BookList(generics.ListAPIView):
-    queryset = Author.objects.all()
-    serializer_class = AuthorSerializer
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            permission_classes = []  # Разрешаем всем
+        else:
+            permission_classes = [IsAdminUser]  # Только админ
+        return [permission() for permission in permission_classes]
+
+class BookList(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'author', 'genre']
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            permission_classes = []  # Разрешаем всем
+        else:
+            permission_classes = [IsAdminUser]  # Только админ
+        return [permission() for permission in permission_classes]

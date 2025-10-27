4ca89e3c86bceb50360401601e031a0d8626618e
Anastasiia <asav32048@gmail.com>
2025-10-28 00:44:51 +0700

    удаление лишней проверки

--- mysite/api/serializers.py
+++ mysite/api/serializers.py
@@ -10,30 +10,3 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['id', 'title', 'author', 'pub_year', 'genre', 'category', 'publisher', 'image', 'file']
-
-    def validate(self, attrs):
-        category = attrs.get('category')
-        if category is None and self.instance:
-            category = self.instance.category
-
-        title = attrs.get('title')
-        author = attrs.get('author')
-        pub_year = attrs.get('pub_year')
-        publisher = attrs.get('publisher')
-
-
-        if category.lower() == 'учебник':
-            existing = Book.objects.filter(
-                title=title or self.instance.title,
-                author=author or self.instance.author,
-                category__iexact='учебник',
-                pub_year=pub_year or self.instance.pub_year,
-                publisher=publisher or self.instance.publisher
-            ).exclude(pk=self.instance.pk if self.instance else None)
-
-            if existing.exists():
-                raise serializers.ValidationError(
-                    'Такой учебник уже существует (по издательству и году).'
-                )
-
-        return attrs
